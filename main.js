e5da4ff3b6129f9742d98e68d99c33908aaa85e0
M00R3D <jobmurdan@hotmail.com>
2025-05-26 22:58:54 -0700

    logica nave

--- main.js
+++ main.js
@@ -1,29 +1,52 @@
+let nave;
 
 function setup()
 {
-    class Nave {
-    constructor(x, y, w, h) 
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        mover() 
-        {
-            this.x += random(-2, 2);
-            this.y += random(-2, 2);
-        }
-
-        mostrar() 
-        {
-            ellipse(this.x, this.y, 20, 20);
-        }
-    }
     createCanvas(1240, 760);
+    let naveX = width / 2;
+    let naveY = height - 22;
+    let naveW = 60;
+    let naveH = 30;
+    nave = new Nave(naveX, naveY, naveW, naveH);
 }
 
 
 function draw() 
 {
     background(5, 0, 14);
+    nave.mover();
+    nave.mostrar();
+    
+}
+
+class Nave 
+{
+    constructor(x, y, w, h) 
+        {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+
+        mover() 
+        {
+            if(keyIsDown('a') && keyIsDown('d')){}
+            else
+                {
+                if (keyIsDown('a')) 
+                    { // A
+                        this.x -= 5;
+                    } 
+                else if (keyIsDown('d')) 
+                    { // D
+                        this.x += 5;
+                    }
+                }
+        }
+
+        mostrar() 
+        {
+            ellipse(this.x, this.y, this.w, this.h);
+        }
 }
