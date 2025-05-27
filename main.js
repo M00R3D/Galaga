fb4b8f4c8239f6a669eb829e87c3edb3cf6c9189
M00R3D <jobmurdan@hotmail.com>
2025-05-27 01:35:36 -0700

    animacion de la nave lista

--- main.js
+++ main.js
@@ -1,60 +1,70 @@
 let nave;
-let imgNave;
+let imgNave, imgNave2;
+let c = 6;
+let frameNave = 0;
 
-
-function setup()
-{
+async function setup() {
     createCanvas(1240, 760);
-    loadImage('recursos/nave.png', function(img) {
-    imgNave = img;
+    imgNave = await loadImageAsync('recursos/nave1.png');
+    imgNave2 = await loadImageAsync('recursos/nave2.png');
     nave = new Nave(width / 2, height - 100, 60, 64, imgNave);
-  }, 
- 
-  function() {
-    console.error('Error: No se pudo cargar la imagen.');
-  });
 }
 
-
-function draw() 
-{
+function draw() {
     background(5, 0, 14);
+
     if (nave) {
         nave.mover();
         nave.mostrar();
     }
-    
+
+  // Animación 
+    if (c > 0) {
+        c--;
+    } else {
+        c = 6;
+    if (frameNave === 0) {
+        frameNave = 1;
+        nave.img = imgNave2;
+    } else {
+        frameNave = 0;
+        nave.img = imgNave;
+    }
+    }
 }
 
-class Nave 
-{
-    constructor(x, y, w, h, img) 
-        {
-            this.x = x;
-            this.y = y;
-            this.w = w;
-            this.h = h;
-            this.img = img;
-        }
+class Nave {
+constructor(x, y, w, h, img) 
+    {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.img = img;
+    }
 
-        mover() 
-        {
-            if(keyIsDown('a') && keyIsDown('d')){}
-            else
-                {
-                if (keyIsDown('a')) 
-                    { // A
-                        this.x -= 5;
-                    } 
-                else if (keyIsDown('d')) 
-                    { // D
-                        this.x += 5;
-                    }
-                }
-        }
+    mover() 
+    {
+        if (keyIsDown('a') && keyIsDown('d')) {return;}
 
-        mostrar() 
-        {
-            image(this.img, this.x, this.y, this.w, this.h);
-        }
+        if (keyIsDown('a')) {this.x -= 5;}
+        else if (keyIsDown('d')) {this.x += 5;}
+
+        this.x = constrain(this.x, 0, width - this.w);
+    }
+
+    mostrar() {
+        image(this.img, this.x, this.y, this.w, this.h);
+    }
+}
+
+
+
+
+
+
+function loadImageAsync(path) {
+    return new Promise((resolve, reject) => {
+        loadImage(path, resolve, reject);
+    });
 }
