ac8f4e879ad77d5ae8bd9ce59c4316805fe6c9cc
M00R3D <jobmurdan@hotmail.com>
2025-05-26 22:21:26 -0700

    cheatsheet de p5, y agrego background oscuro

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
+
 function setup()
 {
-    
+    class Nave {
+    constructor(x, y, w, h) 
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        mover() 
+        {
+            this.x += random(-2, 2);
+            this.y += random(-2, 2);
+        }
+
+        mostrar() 
+        {
+            ellipse(this.x, this.y, 20, 20);
+        }
+    }
+    createCanvas(1240, 760);
 }
 
 
 function draw() 
 {
-
-
+    background(5, 0, 14);
 }
